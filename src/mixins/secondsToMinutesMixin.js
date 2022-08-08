1d5af382a377ea05bb69078328d57f5d9a1aaa92
prosolowski <pavelrosolovski@gmail.com>
2022-08-08 21:34:23 +0200

    add dist

--- src/mixins/secondsToMinutesMixin.js
+++
@@ -1,9 +0,0 @@
-export default {
-  methods: {
-    secondsToMinutes(e) {
-      const m = Math.floor(e % 3600 / 60).toString().padStart(2,'0'),
-            s = Math.floor(e % 60).toString().padStart(2,'0');
-      return m + ':' + s;
-    }
-  }
-};
